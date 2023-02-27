b067905920ce28d05cf61f4f75b9e0f01f20b9a2
xwebz <126299896+xwebz@users.noreply.github.com>
2023-02-26 21:50:47 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+// PAGE BODY
+const body = document.body;
+
 // THE 2 BLOCKS OF THE WEBSITE: [MENU - MAIN CONTENT]
 const menu        = document.getElementById('menu');
 const mainContent = document.getElementById('main-content');
@@ -13,6 +16,11 @@ const menuButton     = document.getElementById('menu-submit');
 
 // MAIN CONTENT ELEMENTS: [CHANGE PREFERENCES BUTTON - CONTAINER WITH LOGO - ETC..................]
 const changeButton = document.getElementById('preferences-btn');
+const logoBox      = document.getElementById('logo-container');
+const container0   = document.getElementById('container0');
+const container1   = document.getElementById('container1');
+const container2   = document.getElementById('container2');
+const container3   = document.getElementById('container3');
 
 // INITIAL VISIBILITY AND OPACITY
 window.onload = function() {
@@ -46,7 +54,38 @@ window.onload = function() {
         menuButton.style.visibility = 'visible';
         menuButton.style.opacity = 1;
     }, 7000);
+    // MAIN CONTENT ELEMENTS HIDE
+    mainContent.remove();
+    setTimeout(() => {
+        changeButton.style.visibility = 'hidden';
+        changeButton.style.opacity    = 0;
+    }, 0);
+    setTimeout(() => {
+        logoBox.style.visibility = 'hidden';
+        logoBox.style.opacity    = 0;
+    }, 0);
+    setTimeout(() => {
+        container0.style.visibility = 'hidden';
+        container0.style.opacity    = 0;
+    }, 0);
+    setTimeout(() => {
+        container1.style.visibility = 'hidden';
+        container1.style.opacity    = 0;
+    }, 0);
+    setTimeout(() => {
+        container2.style.visibility = 'hidden';
+        container2.style.opacity    = 0;
+    }, 0);
+    setTimeout(() => {
+        container3.style.visibility = 'hidden';
+        container3.style.opacity    = 0;
+    }, 0);
 }
+
+
+
+
+
 
 // VISIBILITY AND OPACITY AFTER "OPEN INVITATION" IS CLICKED
 menuButton.addEventListener('click', function(event){
@@ -57,6 +96,7 @@ menuButton.addEventListener('click', function(event){
     mainContent.style.visibility = 'visible';
     mainContent.style.opacity    = 1;
     // MENU ELEMENTS HIDE
+    menu.remove();
     setTimeout(() => {
         menuLogo.style.visibility = 'hidden';
         menuLogo.style.opacity    = 0;
@@ -81,6 +121,32 @@ menuButton.addEventListener('click', function(event){
         menuButton.style.visibility = 'hidden';
         menuButton.style.opacity = 0;
     }, 0);
+    // MAIN CONTENT ELEMENTS SHOW UP
+    body.appendChild(mainContent);
+    setTimeout(() => {
+        changeButton.style.visibility = 'visible';
+        changeButton.style.opacity    = 1;
+    }, 1500);
+    setTimeout(() => {
+        logoBox.style.visibility = 'visible';
+        logoBox.style.opacity    = 1;
+    }, 2000);
+    setTimeout(() => {
+        container0.style.visibility = 'visible';
+        container0.style.opacity    = 1;
+    }, 3000);
+    setTimeout(() => {
+        container1.style.visibility = 'visible';
+        container1.style.opacity    = 1;
+    }, 4000);
+    setTimeout(() => {
+        container2.style.visibility = 'visible';
+        container2.style.opacity    = 1;
+    }, 5000);
+    setTimeout(() => {
+        container3.style.visibility = 'visible';
+        container3.style.opacity    = 1;
+    }, 6000);
 })
 
 // VISIBILITY AND OPACITY AFTER "CHANGE LANGUAGE / LOCATION" IS CLICKED
@@ -92,6 +158,7 @@ changeButton.addEventListener('click', function(event){
     mainContent.style.visibility = 'hidden';
     mainContent.style.opacity    = 0;
     // MENU ELEMENTS SHOW UP
+    body.appendChild(menu);
     setTimeout(() => {
         menuLogo.style.visibility = 'visible';
         menuLogo.style.opacity    = 1;
@@ -116,5 +183,31 @@ changeButton.addEventListener('click', function(event){
         menuButton.style.visibility = 'visible';
         menuButton.style.opacity = 1;
     }, 7000);
+    // MAIN CONTENT ELEMENTS HIDE
+    mainContent.remove();
+    setTimeout(() => {
+        changeButton.style.visibility = 'hidden';
+        changeButton.style.opacity    = 0;
+    }, 0);
+    setTimeout(() => {
+        logoBox.style.visibility = 'hidden';
+        logoBox.style.opacity    = 0;
+    }, 0);
+    setTimeout(() => {
+        container0.style.visibility = 'hidden';
+        container0.style.opacity    = 0;
+    }, 0);
+    setTimeout(() => {
+        container1.style.visibility = 'hidden';
+        container1.style.opacity    = 0;
+    }, 0);
+    setTimeout(() => {
+        container2.style.visibility = 'hidden';
+        container2.style.opacity    = 0;
+    }, 0);
+    setTimeout(() => {
+        container3.style.visibility = 'hidden';
+        container3.style.opacity    = 0;
+    }, 0);
 })
 
