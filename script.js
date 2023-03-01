6634837fe0e8ba82d9eccf5f9a8eeea0effe6b4e
xwebz <126299896+xwebz@users.noreply.github.com>
2023-03-01 00:09:58 -0600

    Update script.js

--- script.js
+++ script.js
@@ -34,6 +34,15 @@ menuButton.addEventListener('click', function(event){
         body.appendChild(mainContentEnglish);
         const txt1       = document.getElementById('text1');
         txt1.textContent = `Welcome to Our ${location} Event`;
+        const changeButton = document.getElementById('preferences-btn');
+        changeButton.addEventListener('click', function(event){
+        event.preventDefault();
+        menu.remove();
+        mainContentEnglish.remove();
+        mainContentFrench.remove();
+        mainContentSpanish.remove();
+        body.appendChild(menu);
+});
     }
     if (language === 'french'){
         mainContentEnglish.remove();
@@ -41,6 +50,15 @@ menuButton.addEventListener('click', function(event){
         body.appendChild(mainContentFrench);
         const txt1       = document.getElementById('text1');
         txt1.textContent = `Bienvenue à notre événement de présentation à ${location}`;
+        const changeButton = document.getElementById('preferences-btn');
+        changeButton.addEventListener('click', function(event){
+        event.preventDefault();
+        menu.remove();
+        mainContentEnglish.remove();
+        mainContentFrench.remove();
+        mainContentSpanish.remove();
+        body.appendChild(menu);
+});
     }
     if (language === 'spanish'){
         mainContentEnglish.remove();
@@ -48,6 +66,15 @@ menuButton.addEventListener('click', function(event){
         body.appendChild(mainContentSpanish);
         const txt1       = document.getElementById('text1');
         txt1.textContent = `Bienvenido(a) a Nuestro Evento de Presentación en ${location}`;
+        const changeButton = document.getElementById('preferences-btn');
+        changeButton.addEventListener('click', function(event){
+        event.preventDefault();
+        menu.remove();
+        mainContentEnglish.remove();
+        mainContentFrench.remove();
+        mainContentSpanish.remove();
+        body.appendChild(menu);
+});
     }
     // Date and time depending on Language and Event Location
     const txt19 = document.getElementById('text19');
